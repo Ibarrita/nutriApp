ddba3dc53cbe0095c2e436c085ed4a347d2c1d3e
José Ibarra <204079627+Ibarrita@users.noreply.github.com>
2025-11-26 10:23:41 -0500

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
 import requests
 
 app = Flask(__name__)
@@ -134,6 +134,39 @@ def gct():
                 elif actividad == "intensa":
                     gct = tmb * 2.2
     return render_template("/herramientas/gct.html", gct=gct)
+
+@app.route("/recetas")
+def recetas():
+    return render_template("recetas.html")
+
+@app.route("/search", methods=["POST"])
+def search():
+    recipe_name = request.form.get("recipe", "").strip().lower()
+    
+    if not recipe_name:
+        flash("Por favor, ingresa un alimento", "error")
+        return redirect(url_for("recetas"))
+    try:
+        response = requests.get(f"{API}?apiKey={KEY}&query={recipe_name}&maxFat=25&number=1")
+        
+        if response.status_code == 200:
+            recipe_data = response.json()
+            results = recipe_data.get("results", [])
+            if not results:
+                flash("No se encontró el alimento", "error")
+                return redirect(url_for("recetas"))
+            resultados = results[0]
+            recipe_info = {
+                "title": resultados.get("title"),
+                "image": resultados.get("image")
+            }
+            return render_template("alimento.html", recipe=recipe_info)
+        else:
+            flash("Error al contactar con la API", "error")
+            return redirect(url_for("recetas"))
+    except requests.exceptions.RequestException as e:
+        flash("No se pudo contactar con la API", "error")
+        return redirect(url_for("recetas"))
 
 if __name__ == "__main__":
     app.run(debug=True)
